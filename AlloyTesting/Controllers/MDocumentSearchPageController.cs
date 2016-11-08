1852bc64ead139c57f58749a782a111f2a0d9767
David Zhao <ntierontime@gmail.com>
2016-11-08 09:38:51 -0500

    EPiServer.Search

--- AlloyTesting/Controllers/MDocumentSearchPageController.cs
+++ AlloyTesting/Controllers/MDocumentSearchPageController.cs
@@ -16,6 +16,7 @@ using EPiServer.Find.Cms;
 
 using Alloy.Models.Media;
 using EPiServer.Web.Mvc;
+using EPiServer.Find.Framework;
 
 namespace Alloy.Controllers
 {
@@ -32,109 +33,125 @@ namespace Alloy.Controllers
             this.requiredClientResourceList = requiredClientResourceList;
         }
 
-        [ValidateInput(false)]
         public ActionResult Index(MDocumentSearchPage currentPage, string q)
         {
-            var model = new MDocumentSearchPageViewModel(currentPage);
-            if (string.IsNullOrEmpty(q))
+            var model = new MDocumentSearchPageViewModel(currentPage, q);
+            if (String.IsNullOrEmpty(q))
             {
                 return View(model);
             }
             
-            //detect if serviceUrl and/or defaultIndex is configured.
-            model.IsConfigured = SearchIndexIsConfigured(EPiServer.Find.Configuration.GetConfiguration());
+            //var unifiedSearch = SearchClient.Instance.UnifiedSearchFor(q);
 
-            if (model.IsConfigured && !string.IsNullOrWhiteSpace(model.Query))
-            {
-                var query = BuildQuery(model);
+            var unifiedSearch = SearchClient.Instance.Search<MDocumentFileBase>().For(q);
 
-                //Create a hit specification to determine display based on values entered by an editor on the search page.
-                var hitSpec = new HitSpecification
-                {
-                    HighlightTitle = model.CurrentPage.HighlightTitles,
-                    HighlightExcerpt = model.CurrentPage.HighlightExcerpts,
-                    ExcerptLength = model.ExcerptLength
-                };
-
-                try
-                {
-                    model.Hits = query.GetResult(hitSpec);
-                }
-                catch (WebException wex)
-                {
-                    model.IsConfigured = wex.Status != WebExceptionStatus.NameResolutionFailure;
-                }
-            }
-
-            RequireClientResources();
-
+            model.Results = unifiedSearch.GetContentResult<MDocumentFileBase>();
             return View(model);
         }
 
+        //[ValidateInput(false)]
+        //public ActionResult Index(MDocumentSearchPage currentPage, string q)
+        //{
+        //    var model = new MDocumentSearchPageViewModel(currentPage);
+        //    if (string.IsNullOrEmpty(q))
+        //    {
+        //        return View(model);
+        //    }
+            
+        //    //detect if serviceUrl and/or defaultIndex is configured.
+        //    model.IsConfigured = SearchIndexIsConfigured(EPiServer.Find.Configuration.GetConfiguration());
 
-        private ITypeSearch<ISearchContent> BuildQuery(MDocumentSearchPageViewModel model)
-        {
-            var queryFor = searchClient.UnifiedSearch().For(model.Query);
+        //    if (model.IsConfigured && !string.IsNullOrWhiteSpace(model.Query))
+        //    {
+        //        var query = BuildQuery(model);
 
-            if (model.UseAndForMultipleSearchTerms)
-            {
-                queryFor = queryFor.WithAndAsDefaultOperator();
-            }
+        //        //Create a hit specification to determine display based on values entered by an editor on the search page.
+        //        var hitSpec = new HitSpecification
+        //        {
+        //            HighlightTitle = model.CurrentPage.HighlightTitles,
+        //            HighlightExcerpt = model.CurrentPage.HighlightExcerpts,
+        //            ExcerptLength = model.ExcerptLength
+        //        };
 
-            var query = queryFor
-                .UsingSynonyms()
-                .UsingAutoBoost(TimeSpan.FromDays(30))
-                //Include a facet whose value is used to show the total number of hits regardless of section.
-                //The filter here is irrelevant but should match *everything*.
-                .TermsFacetFor(x => x.SearchSection)
-                .FilterFacet("AllSections", x => x.SearchSection.Exists())
-                //Fetch the specific paging page.
-                .Skip((model.PagingPage - 1) * model.CurrentPage.PageSize)
-                .Take(model.CurrentPage.PageSize)
-                //Allow editors (from the Find/Optimizations view) to push specific hits to the top 
-                //for certain search phrases.
-                .ApplyBestBets();
+        //        try
+        //        {
+        //            model.Hits = query.GetResult(hitSpec);
+        //        }
+        //        catch (WebException wex)
+        //        {
+        //            model.IsConfigured = wex.Status != WebExceptionStatus.NameResolutionFailure;
+        //        }
+        //    }
 
-            // obey DNT
-            var doNotTrackHeader = System.Web.HttpContext.Current.Request.Headers.Get("DNT");
-            // Should not track when value equals 1
-            if (doNotTrackHeader == null || doNotTrackHeader.Equals("0"))
-            {
-                query = query.Track();
-            }
+        //    RequireClientResources();
 
-            //If a section filter exists (in the query string) we apply
-            //a filter to only show hits from a given section.
-            if (!string.IsNullOrWhiteSpace(model.SectionFilter))
-            {
-                query = query.FilterHits(x => x.SearchSection.Match(model.SectionFilter));
-            }
-            return query;
-        }
+        //    return View(model);
+        //}
 
-        /// <summary>
-        /// Checks if service url and index are configured
-        /// </summary>
-        /// <param name="configuration">Find configuration</param>
-        /// <returns>True if configured, false otherwise</returns>
-        private bool SearchIndexIsConfigured(EPiServer.Find.Configuration configuration)
-        {
-            return (!configuration.ServiceUrl.IsNullOrEmpty()
-                    && !configuration.ServiceUrl.Contains("YOUR_URI")
-                    && !configuration.DefaultIndex.IsNullOrEmpty()
-                    && !configuration.DefaultIndex.Equals("YOUR_INDEX"));
-        }
 
-        /// <summary>
-        /// Requires the client resources used in the view.
-        /// </summary>
-        private void RequireClientResources()
-        {
-            // jQuery.UI is used in autocomplete example.
-            // Add jQuery.UI files to existing client resource bundles or load it from CDN or use any other alternative library.
-            // We use local resources for demo purposes without Internet connection.
-            requiredClientResourceList.RequireStyle(VirtualPathUtilityEx.ToAbsolute("~/Static/css/jquery-ui.css"));
-            requiredClientResourceList.RequireScript(VirtualPathUtilityEx.ToAbsolute("~/Static/js/jquery-ui.js")).AtFooter();
-        }
+        //private ITypeSearch<ISearchContent> BuildQuery(MDocumentSearchPageViewModel model)
+        //{
+        //    var queryFor = searchClient.UnifiedSearch().For(model.Query);
+
+        //    if (model.UseAndForMultipleSearchTerms)
+        //    {
+        //        queryFor = queryFor.WithAndAsDefaultOperator();
+        //    }
+
+        //    var query = queryFor
+        //        .UsingSynonyms()
+        //        .UsingAutoBoost(TimeSpan.FromDays(30))
+        //        //Include a facet whose value is used to show the total number of hits regardless of section.
+        //        //The filter here is irrelevant but should match *everything*.
+        //        .TermsFacetFor(x => x.SearchSection)
+        //        .FilterFacet("AllSections", x => x.SearchSection.Exists())
+        //        //Fetch the specific paging page.
+        //        .Skip((model.PagingPage - 1) * model.CurrentPage.PageSize)
+        //        .Take(model.CurrentPage.PageSize)
+        //        //Allow editors (from the Find/Optimizations view) to push specific hits to the top 
+        //        //for certain search phrases.
+        //        .ApplyBestBets();
+
+        //    // obey DNT
+        //    var doNotTrackHeader = System.Web.HttpContext.Current.Request.Headers.Get("DNT");
+        //    // Should not track when value equals 1
+        //    if (doNotTrackHeader == null || doNotTrackHeader.Equals("0"))
+        //    {
+        //        query = query.Track();
+        //    }
+
+        //    //If a section filter exists (in the query string) we apply
+        //    //a filter to only show hits from a given section.
+        //    if (!string.IsNullOrWhiteSpace(model.SectionFilter))
+        //    {
+        //        query = query.FilterHits(x => x.SearchSection.Match(model.SectionFilter));
+        //    }
+        //    return query;
+        //}
+
+        ///// <summary>
+        ///// Checks if service url and index are configured
+        ///// </summary>
+        ///// <param name="configuration">Find configuration</param>
+        ///// <returns>True if configured, false otherwise</returns>
+        //private bool SearchIndexIsConfigured(EPiServer.Find.Configuration configuration)
+        //{
+        //    return (!configuration.ServiceUrl.IsNullOrEmpty()
+        //            && !configuration.ServiceUrl.Contains("YOUR_URI")
+        //            && !configuration.DefaultIndex.IsNullOrEmpty()
+        //            && !configuration.DefaultIndex.Equals("YOUR_INDEX"));
+        //}
+
+        ///// <summary>
+        ///// Requires the client resources used in the view.
+        ///// </summary>
+        //private void RequireClientResources()
+        //{
+        //    // jQuery.UI is used in autocomplete example.
+        //    // Add jQuery.UI files to existing client resource bundles or load it from CDN or use any other alternative library.
+        //    // We use local resources for demo purposes without Internet connection.
+        //    requiredClientResourceList.RequireStyle(VirtualPathUtilityEx.ToAbsolute("~/Static/css/jquery-ui.css"));
+        //    requiredClientResourceList.RequireScript(VirtualPathUtilityEx.ToAbsolute("~/Static/js/jquery-ui.js")).AtFooter();
+        //}
     }
 }
